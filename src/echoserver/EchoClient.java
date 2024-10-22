9a722c54097b9ef307a18d1bba96039969dc674c
mahon479 <mahon479@morris.umn.edu>
2024-10-22 11:04:30 -0500

    Wrote code for the client and server but the tests appear to be failing.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,38 @@
 package echoserver;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class EchoClient {
+    public static final int portNumber = 6013;
+
+    public static void main(String[] args) throws IOException {
+        String server = (args.length == 0) ? "127.0.0.1" : args[0];
+
+        try (Socket socket = new Socket(server, portNumber)) {
+            // Get the input and output streams for the socket
+            InputStream input = socket.getInputStream();
+            OutputStream output = socket.getOutputStream();
+
+            // Read bytes from System.in and send them to the server
+            int byteRead;
+            while ((byteRead = System.in.read()) != -1) {
+                output.write(byteRead);
+                output.flush();
+            }
+
+            // Shutdown the output to signal the server we're done sending data
+            socket.shutdownOutput();
+
+            // Read the echoed bytes from the server and print them to System.out
+            while ((byteRead = input.read()) != -1) {
+                System.out.write(byteRead);
+            }
+            System.out.flush();
+        } catch (IOException ioe) {
+            System.err.println("Client error: " + ioe.getMessage());
+        }
+    }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,38 @@
 package echoserver;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class EchoServer {
+    public static final int portNumber = 6013;
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            // Run forever, which is common for server style services
+            while (true) {
+                // Wait until someone connects
+                try (Socket clientSocket = serverSocket.accept()) {
+                    System.out.println("Got a request!");
+
+                    // Get the input and output streams for the client socket
+                    InputStream input = clientSocket.getInputStream();
+                    OutputStream output = clientSocket.getOutputStream();
+
+                    // Read bytes from the client and write them back to the client
+                    int byteRead;
+                    while ((byteRead = input.read()) != -1) {
+                        output.write(byteRead);
+                        output.flush();
+                    }
+                } catch (IOException e) {
+                    System.err.println("Client connection error: " + e.getMessage());
+                }
+            }
+        } catch (IOException ioe) {
+            System.err.println("Server error: " + ioe.getMessage());
+        }
+    }
 }
